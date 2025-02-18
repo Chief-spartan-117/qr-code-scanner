1749be37e205c87208d93ed21680fb7afda66b41
Chief-spartan-117 <shresthasagar861@gmail.com>
2025-02-19 01:19:49 +0545

    QR Code Generation logic

--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -1,8 +1,38 @@
 import { json, type RequestHandler } from "@sveltejs/kit";
-
+import Cryptr from "cryptr";
+import qrcode from "qrcode";
+import { SECRET_KEY } from "$env/static/private";
+import prisma from "$lib/prisma";
 
 export const POST: RequestHandler = async ({ request }) => {
-    const { name, email, phoneNumber } = await request.json();
 
-    return json({ name, email, phoneNumber });
+    try {
+        const { name, email, phoneNumber } = await request.json();
+
+        if (!name || !email || !phoneNumber) {
+            return json({ "message": "Fields missing" });
+        }
+
+        const crypter = new Cryptr(SECRET_KEY);
+
+        const encData = crypter.encrypt(`${SECRET_KEY}|sp|${email}`);
+
+        const qrCodeData = await qrcode.toDataURL(encData);
+
+
+        console.log(name, email, phoneNumber);
+
+        await prisma.qr.create({
+            data: {
+                email,
+                name,
+                phoneNumber,
+            },
+        });
+
+        return json(qrCodeData);
+    }
+    catch (err) {
+        return json(err);
+    }
 };
